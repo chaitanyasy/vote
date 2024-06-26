426d3df3282d690464644ccde579a2d3b5bbd033
chaitanyasy <kodavati.chaitanya@gmail.com>
2024-06-26 10:50:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "RCB")
+option_b = os.getenv('OPTION_B', "CSK")
 hostname = socket.gethostname()
 version = 'v1'
 
